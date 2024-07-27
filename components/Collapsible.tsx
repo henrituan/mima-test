4414836ce781d7317d4176050a266319898517b0
Henri Tuan <henri.tuan77@gmail.com>
2024-07-27 20:07:08 +0200

    chore: prettier

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -2,9 +2,10 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
 
+import { Colors } from '@/constants/Colors';
+
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { Colors } from '@/constants/Colors';
 
 export function Collapsible({
   children,
