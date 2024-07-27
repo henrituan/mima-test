a59a029bdf01567c93827edc275527e1d61cbc96
Henri Tuan <henri.tuan77@gmail.com>
2024-07-27 18:13:03 +0200

    chore: eslint import order

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,55 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: '@/constants',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/services',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/stores/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/hooks',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/app',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/components/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
 };
