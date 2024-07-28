ef34a86598ebb3791f53c4c333c76875b4c0f994
Henri Tuan <henri.tuan77@gmail.com>
2024-07-28 23:21:13 +0200

    feat: home cover

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -11,8 +11,8 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/home-cover.png')}
+          style={styles.coverImage}
         />
       }
     >
@@ -64,11 +64,11 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  coverImage: {
+    height: '100%',
+    width: '100%',
+    // bottom: 0,
+    // left: 0,
+    // position: 'absolute',
   },
 });
